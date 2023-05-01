0e50ce01bb9f47d64cc1f0ab46994b964564c7f1
Daria Lyubaeva <lyubaevadaria@gmail.com>
2023-05-01 17:55:16 +0200

    excluded topical words from wordclouds ("healthcare", "ai", "medical") to increase the weights of topics discussed, changed sentiment scoring to be neutral from -0.1 to 0.1 (accommodating for investment posts that were slighly negative), saving the texts again with their sentiment scores for post-analysis, removed links from raw text before calculating sentiment

--- sentiment.py
+++ sentiment.py
@@ -8,6 +8,10 @@ from textblob import TextBlob
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
+def remove_links(text):
+    text = re.sub(r'http\S+', '', text)
+    return text
+
 
 def sentiment_analysis(text):
     text_str = ' '.join(text)
@@ -25,19 +29,22 @@ def extract_topics(corpus, num_topics=20):
 
 
 def preprocess_text(text):
-    text = re.sub(r'\W', ' ', text)
-    text = re.sub(r'\s+', ' ', text)
+    text = re.sub(r'\n', ' ', str(text))
+    text = re.sub(r'\t', ' ', str(text))
+    text = re.sub(r'\W', ' ', str(text))
+    text = re.sub(r'\s+', ' ', str(text))
     text = text.lower()
     words = text.split()
     stopword_extended = stopwords.words("english")
     stopword_extended.extend(
         ["http", "https", "www", "com", "org", "net", "edu", "reddit", "redditcom", "redditcomr", "redditcomrall",
-         "en"])
+         "en", "x200b", "png", "webp", "jpg", "jpeg", "gif", "html", "n", "u",
+         "healthcare", "ai", "need", "medical"])
     words = [word for word in words if word not in stopword_extended]
     return words
 
 
-folder_path = "reddit_posts/new_query"
+folder_path = "reddit_posts/medical_ai"
 minimum_karma = 50
 
 combined_data = []
@@ -61,18 +68,6 @@ print(f"Total number of credible posts in combined dataset: {len(credible_data)}
 print(f"Total number of comments for all credible posts: {sum([len(post['comments']) for post in credible_data])}")
 
 
-def preprocess_text(text):
-    text = re.sub(r'\W', ' ', text)
-    text = re.sub(r'\s+', ' ', text)
-    text = text.lower()
-    words = text.split()
-    stopword_extended = stopwords.words("english")
-    stopword_extended.extend(
-        ["http", "https", "www", "com", "org", "net", "edu", "reddit", "redditcom", "redditcomr", "redditcomrall",
-         "en", "x200b", "png", "webp", "jpg", "jpeg", "gif", "html"])
-    words = [word for word in words if word not in stopword_extended]
-    return words
-
 
 def generate_wordcloud(corpus, type="positive"):
     text = ' ||| '.join([' '.join(phrase) for phrase in corpus])
@@ -86,28 +81,69 @@ def generate_wordcloud(corpus, type="positive"):
     wordcloud.to_file(f"wordclouds/wordcloud_{type}.png")
 
 
-preprocessed_titles = [preprocess_text(post["title"] + " " + post["text"]) for post in credible_data]
-preprocessed_comments = [preprocess_text(comment["text"]) for post in credible_data for comment in post["comments"]]
+# preprocessed_titles = [preprocess_text(post["title"] + " " + post["text"]) for post in credible_data]
+# preprocessed_comments = [preprocess_text(comment["text"]) for post in credible_data for comment in post["comments"]]
 
-topics = extract_topics(preprocessed_titles + preprocessed_comments)
+full_texts = [remove_links(post["title"]) + " " + remove_links(post["text"]) for post in credible_data]
+full_texts.append([remove_links(comment["text"]) for post in credible_data for comment in post["comments"]])
 
-sentiments = [sentiment_analysis(text) for text in preprocessed_titles + preprocessed_comments]
 
-positive_phrases = [text for text, sentiment in zip(preprocessed_titles + preprocessed_comments, sentiments) if
-                    sentiment > 0]
-negative_phrases = [text for text, sentiment in zip(preprocessed_titles + preprocessed_comments, sentiments) if
-                    sentiment < 0]
-neutral_phrases = [text for text, sentiment in zip(preprocessed_titles + preprocessed_comments, sentiments) if
-                   sentiment == 0]
+sentiments = [sentiment_analysis(text) for text in full_texts]
+# plot the distribution of sentiments, percentage of positive, negative and neutral sentiments
+plt.figure(figsize=(10, 5))
+
+num_positive = len([sentiment for sentiment in sentiments if sentiment > 0.1])
+num_negative = len([sentiment for sentiment in sentiments if sentiment < -0.1])
+num_neutral = len([sentiment for sentiment in sentiments if sentiment <= 0.1 and sentiment >= -0.1])
+
+total_phrases = num_positive + num_negative + num_neutral
+positive_percentage = (num_positive / total_phrases) * 100
+negative_percentage = (num_negative / total_phrases) * 100
+neutral_percentage = (num_neutral/ total_phrases) * 100
+
+labels = ['Positive', 'Negative', 'Neutral']
+sizes = [positive_percentage, negative_percentage, neutral_percentage]
+colors = ['#99ff99', '#ff9999', '#66b3ff']
+explode = (0.1, 0.1, 0.1)
+
+plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=90)
+
+plt.axis('equal')
+
+plt.show()
+
+
+positive_phrases = [text for text, sentiment in zip(full_texts, sentiments) if sentiment > 0.1]
+negative_phrases = [text for text, sentiment in zip(full_texts, sentiments) if sentiment < -0.1]
+neutral_phrases = [text for text, sentiment in zip(full_texts, sentiments) if sentiment <= 0.1 and sentiment >= -0.1]
+
+# save positive, negative and neutral phrases to json files, including their sentiments
+with open("positive_phrases_with_sentiment.json", "w") as file:
+    json.dump([(text, sentiment) for text, sentiment in zip(positive_phrases, sentiments) if sentiment > 0], file)
+
+with open("negative_phrases_with_sentiment.json", "w") as file:
+    json.dump([(text, sentiment) for text, sentiment in zip(negative_phrases, sentiments) if sentiment < 0], file)
+
+with open("neutral_phrases_with_sentiment.json", "w") as file:
+    json.dump([(text, sentiment) for text, sentiment in zip(neutral_phrases, sentiments) if sentiment == 0], file)
+
+
+
+preprocessed_positive_phrases = [preprocess_text(text) for text in positive_phrases]
+preprocessed_negative_phrases = [preprocess_text(text) for text in negative_phrases]
+preprocessed_neutral_phrases = [preprocess_text(text) for text in neutral_phrases]
+topics = extract_topics(preprocessed_positive_phrases + preprocessed_negative_phrases + preprocessed_neutral_phrases, num_topics=10)
+
+
 
 print("Positive word cloud:")
-generate_wordcloud(positive_phrases, type="positive")
+generate_wordcloud(preprocessed_positive_phrases, type="positive")
 
 print("Negative word cloud:")
-generate_wordcloud(negative_phrases, type="negative")
+generate_wordcloud(preprocessed_negative_phrases, type="negative")
 
 print("Neutral word cloud:")
-generate_wordcloud(neutral_phrases, type="neutral")
+generate_wordcloud(preprocessed_neutral_phrases, type="neutral")
 
 results = {
     "topics": topics,
@@ -118,6 +154,5 @@ results = {
     }
 }
 
-# Save the results to a JSON file
 with open("results.json", "w") as outfile:
     json.dump(results, outfile, indent=4)
